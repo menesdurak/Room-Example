5d81383db375e5aef2441acc22df9f719c5bef01
menesdurak <m.enes.durak@gmail.com>
2022-12-01 21:37:07 +0300

    Fragments created

---
+++ app/src/main/java/com/menesdurak/roomexample/view/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.menesdurak.roomexample.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.menesdurak.roomexample.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+    }
+}
