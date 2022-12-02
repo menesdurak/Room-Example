5c6fd86f0d25cf27f008c9baae621eedf577eb57
menesdurak <m.enes.durak@gmail.com>
2022-12-02 12:51:00 +0300

    Viewmodel changed and added viewmodelfactory

--- app/src/main/java/com/menesdurak/roomexample/view/dictionary/DictionaryFragment.kt
+++ app/src/main/java/com/menesdurak/roomexample/view/dictionary/DictionaryFragment.kt
@@ -5,15 +5,24 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import com.menesdurak.roomexample.R
 import com.menesdurak.roomexample.databinding.FragmentDictionaryBinding
+import com.menesdurak.roomexample.model.Word
+import com.menesdurak.roomexample.view.WordApplication
 
 class DictionaryFragment : Fragment() {
     private var _binding: FragmentDictionaryBinding? = null
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+
+    private val viewModel: WordViewModel by activityViewModels {
+        BusScheduleViewModelFactory(
+            (activity?.application as WordApplication).database.getWordDao()
+        )
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -22,6 +31,13 @@ class DictionaryFragment : Fragment() {
     ): View? {
         _binding = FragmentDictionaryBinding.inflate(inflater, container, false)
         val view = binding.root
+
+        val word1 = Word("Red", "Kırmızı")
+        val word2 = Word("Blue", "Mavi")
+
+        viewModel.addWord(word1)
+        viewModel.addWord(word2)
+
         return view
     }
 
--- app/src/main/java/com/menesdurak/roomexample/view/dictionary/WordViewModel.kt
+++ app/src/main/java/com/menesdurak/roomexample/view/dictionary/WordViewModel.kt
@@ -1,46 +1,56 @@
 package com.menesdurak.roomexample.view.dictionary
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.*
+import com.menesdurak.roomexample.database.WordDao
 import com.menesdurak.roomexample.model.Word
 import com.menesdurak.roomexample.repository.WordRepository
 import kotlinx.coroutines.launch
 
-class WordViewModel(private val repository: WordRepository) : ViewModel() {
+class WordViewModel(private val wordDao: WordDao) : ViewModel() {
 
     private val mutableWordList: MutableLiveData<List<Word>> = MutableLiveData()
     val wordList: LiveData<List<Word>>
         get() = mutableWordList
 
-    fun insertWord(word: Word) {
+    fun addWord(word: Word) {
         viewModelScope.launch {
-            repository.addWord(word)
+            wordDao.addWord(word)
         }
     }
 
     fun updateWord(word: Word) {
         viewModelScope.launch {
-            repository.updateWord(word)
+            wordDao.updateWord(word)
         }
     }
 
     fun deleteWord(word: Word) {
         viewModelScope.launch {
-            repository.deleteWord(word)
+            wordDao.deleteWord(word)
         }
     }
 
     fun deleteAllWords() {
         viewModelScope.launch {
-            repository.deleteAllWords()
+            wordDao.deleteAllWords()
         }
     }
 
     fun getAllWords() {
         viewModelScope.launch {
-            mutableWordList.value = repository.getAllWords().value
+            mutableWordList.value = wordDao.getAllWords().value
         }
+    }
+}
+
+class BusScheduleViewModelFactory(
+    private val wordDao: WordDao
+) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(WordViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return WordViewModel(wordDao) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
     }
 }
